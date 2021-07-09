6e632aacf695bab6b239e08cedf4b3ae8459ef81
Marlon Johnson <johnsonmarlon18@gmail.com>
2021-07-09 15:42:20 -0700

    feat: add tw-timeline project :)

--- content/data/projects.js
+++ content/data/projects.js
@@ -1,10 +1,17 @@
-//eslint-disable-next-line
+// eslint-disable-next-line
 export default [
   {
     title: '💼 Github Jobs API',
     description:
-      'An application for searching jobs built using the git.jobs API and Next JS',
+      'An application for searching jobs built using the git.jobs API and Next.js',
     code: 'https://github.com/Mvrs/git-jobs',
     live: 'https://git-jobs.mvrs.vercel.app/',
+  },
+  {
+    title: '🐦 tw-timeline',
+    description:
+      'Twitter timeline clone to post random tweets anonymously built with TypeScript, Prisma, and Next.js',
+    code: 'https://github.com/Mvrs/tw-timeline',
+    live: 'http://tw-timeline.vercel.app/',
   },
 ];
