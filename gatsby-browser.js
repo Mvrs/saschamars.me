bff47aa294b1b241e2a44d4c462b31fe5cab286b
Mvrs <johnsonmarlon18@gmail.com>
2022-10-04 09:59:01 -0700

    chore: fix blog code-blocks

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,8 +1,8 @@
-// import React from 'react';
-// import { MDXProvider } from '@mdx-js/react';
-// import { components } from './src/components/mdx/index';
+import { MDXProvider } from '@mdx-js/react';
+import React from 'react';
+import { components } from './src/components/mdx/index';
 
-// // eslint-disable-next-line
-// export const wrapRootElement = ({ element }) => {
-//   return <MDXProvider components={components}>{element}</MDXProvider>;
-// };
+// eslint-disable-next-line
+export const wrapRootElement = ({ element }) => {
+  return <MDXProvider components={components}>{element}</MDXProvider>;
+};
